6abf5a6795cb1e839f1af3af8ea4d275108788ff
Clayde Nhicos A. Arnaiz <112850099+claydearnaiz@users.noreply.github.com>
2023-08-18 12:30:20 +0800

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int length, width, area;
+    
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+       
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+    
+    area = length * width;
+    cout << "The area of the rectangle is: " << area; 
+    
+    return 0;
+}
