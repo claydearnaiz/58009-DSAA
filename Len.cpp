5bd8bf06d2aab6eea66b1dc1b778c5f070af7e6a
Clayde Nhicos A. Arnaiz <112850099+claydearnaiz@users.noreply.github.com>
2023-09-06 13:15:43 +0800

    Update and rename Number 5.cpp to Len.cpp

--- Len.cpp
+++ Len.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main(){
 	vector<int> numbers = {5, 4, 3, 2, 1};
 	cout << "NUMBERS INSIDE THE ARRAY: ";
-    for (int i = 0; i < numbers.size(); i++) 
-	{
+    for (int i = 0; i < numbers.size(); i++) {
         cout << numbers[i] << " ";
     }
-    cout << endl;
-	cout << "LENGTH OF THE ARRAY: " << numbers.size();
+    	cout << endl;
+    	cout << "LENGTH OF THE ARRAY: " << numbers.size();
 	
 	return 0;
 }
